5bb53b7babf86fe65cfab59da46435b2a2c30d39
babaphillips <amandavphillips@icloud.com>
2022-09-09 22:01:14 -0400

    issues with post_url

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-// create connection to our db
+// create connection to our db, pass in your MySQL information for username and password
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
