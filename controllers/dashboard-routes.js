2ed83bff5155c5395aa53785c4b7be31753955b7
babaphillips <amandavphillips@icloud.com>
2022-09-11 19:01:44 -0400

    Fixed Edit bug

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -37,6 +37,7 @@ router.get("/", withAuth, (req, res) => {
 
 router.get("/edit/:id", withAuth, (req, res) => {
   Post.findByPk(req.params.id, {
+    attributes: ["id", "post_text", "title", "created_at"],
     include: [
       {
         model: Comment,
