8f0de3255373436faaf0831b3f9ec3bc33dae46a
babaphillips <amandavphillips@icloud.com>
2022-09-11 17:34:56 -0400

    Login fixed

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,12 +1,13 @@
 async function loginFormHandler(event) {
   event.preventDefault();
-
+  const username = document.querySelector("#username-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
   if (password) {
     const response = await fetch("/api/users/login", {
       method: "post",
       body: JSON.stringify({
+        username,
         password,
       }),
       headers: { "Content-Type": "application/json" },
